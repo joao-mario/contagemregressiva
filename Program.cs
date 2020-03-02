6cbb384b5e4d878ce08321ff0806918c63a47b65
joao mario <jjsp0403@gmail.com>
2020-03-02 19:34:46 -0300

    add projeto

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace contagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preparar...");
+            Console.WriteLine("Apontar...");
+            Console.WriteLine("FOGO!");
+        }
+    }
+}
